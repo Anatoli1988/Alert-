1b9ef1d54d99ea58532097d0937750a1153ef0d4
Anatoli1988 <energetik-ym191@mail.ru>
2019-11-13 15:46:01 +0300

    Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const Notification = (props: any) => {
+    return <div className={"notification " + props.className}>{props.text}</div>
+}
+
+interface IProps {}
+
+type NotificationType = "error" | "success" | "warning"
+
+
+
+interface INotification {
+    id: number
+    title: string
+    type: NotificationType
+}
+
+interface  IState {
+    notifications: Array<INotification>
+
+}
+
+class App extends Component <IProps,IState> {
+    state: IState = {
+        notifications: [ ]
+    }
+
+    addNotification = (type: NotificationType,title: string) =>{
+        let note = {id: (new Date()).getTime(), title: title, type: type}
+        let notifications: Array <INotification> = [...this.state.notifications,note ]
+        this.setState ({
+            notifications: notifications
+        })
+        setTimeout(()=>{
+            this.setState({
+                notifications: this.state.notifications.filter(n => n.id != note.id)
+            })
+        },3000)
+    }
+
+    successClick = () =>{
+        this.addNotification("success", "Yes")
+    }
+
+    errorClick = () =>{
+        this.addNotification("error", "Bad")
+    }
+    warningClick = () =>{
+        this.addNotification("warning", "Attation")
+    }
+
+    render() {
+        let Elements = this.state.notifications.map((n) => <Notification key={n.id} text={n.title} className={n.type}/>)
+
+        return (
+            <div className="App">
+                <button onClick={this.successClick}>
+                    Success
+                </button>
+                <button onClick={this.errorClick}>Error</button>
+                <button onClick={this.warningClick}>Warning</button>
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+                sdjkasbdkasbcdkjasbjkasnkj
+
+                <div className={"notification-block"}>
+                    {Elements}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
